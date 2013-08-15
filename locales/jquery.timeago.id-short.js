c37448f026b7d7dcaf8e4e1a59264c6b773354a6
Rezha Julio <kimiamania11@gmail.com>
2013-08-15 15:37:59 +0700

    Adding Indonesian shortened version

---
+++ locales/jquery.timeago.id-short.js
@@ -0,0 +1,20 @@
+// Indonesian shortened
+jQuery.timeago.settings.strings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: "",
+  suffixFromNow: "",
+  seconds: "1m",
+  minute: "1m",
+  minutes: "%dm",
+  hour: "1j",
+  hours: "%dj",
+  day: "1hr",
+  days: "%dhr",
+  month: "1bln",
+  months: "%dbln",
+  year: "1thn",
+  years: "%dthn",
+  wordSeparator: " ",
+  numbers: []
+};
